c721a3034375c88c0aa171a52817d17b7bcacde3
matiaswasiak <matiaswasiak@gmail.com>
2021-09-01 21:31:01 -0300

    first commit / estructura del proyecto

---
+++ src/team/ed/objects/Product.java
@@ -0,0 +1,48 @@
+package team.ed.objects;
+
+public abstract class Product {
+    private String name;
+    private int amount;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        if (amount < 0) {
+            System.out.println("No puede asignar una cantidad negativa");
+            return;
+        }
+
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            System.out.println("No se puede colocar un precio negativo");
+            return;
+        }
+
+        this.price = price;
+    }
+}
