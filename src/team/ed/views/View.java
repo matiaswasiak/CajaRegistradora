8a98975cd63256164598fe36a994ae97833626d5
matiaswasiak <matiaswasiak@gmail.com>
2021-09-07 20:04:28 -0300

    Menus complementarios y obteniendo cantidad de productos

--- src/team/ed/views/View.java
+++ src/team/ed/views/View.java
@@ -1,5 +1,7 @@
 package team.ed.views;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 
 public class View {
@@ -22,8 +24,74 @@ public class View {
         System.out.print("\t\tDigite la opción: ");
     }
 
+    public static void showGetAmount() {
+        System.out.print("\t\tDigite la cantidad: ");
+    }
+
+    public static void showGetPrice() {
+        System.out.print("\t\tDigite el precio: ");
+    }
+
     public static int getOption() {
         Scanner teclado = new Scanner(System.in);
         return teclado.nextInt();
+    }
+
+    public static int getAmount() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
+
+    public static double getPrice() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextDouble();
+    }
+
+    public static void showBuyHeader() {
+        System.out.println("\t\t*************************************");
+        System.out.println("\t\t*          MENU DE COMPRAS          *");
+        System.out.println("\t\t*************************************");
+        showItemsMenu();
+    }
+
+    public static void showSaleHeader() {
+        System.out.println("\t\t*************************************");
+        System.out.println("\t\t*           MENU DE VENTAS          *");
+        System.out.println("\t\t*************************************");
+        showItemsMenu();
+    }
+
+    public static void showStockHeader() {
+        System.out.println("\t\t****************************************************");
+        System.out.println("\t\t*                    INVENTARIO                    *");
+        System.out.println("\t\t****************************************************");
+        showItemsHeader();
+    }
+
+    public static void showItemsHeader() {
+        System.out.println("\t\t----------------------------------------------------");
+        System.out.println("\t\t* PRODUCTO\t| Cantidad\t| Vr Unitario\t| Vr Total\t*");
+        System.out.println("\t\t----------------------------------------------------");
+    }
+
+    public static void showBuyListHeader() {
+        System.out.println("\t\t****************************************************");
+        System.out.println("\t\t*                     COMPRAS                      *");
+        System.out.println("\t\t****************************************************");
+        showItemsHeader();
+    }
+
+    public static void showSaleListHeader() {
+        System.out.println("\t\t****************************************************");
+        System.out.println("\t\t*                     VENTAS                       *");
+        System.out.println("\t\t****************************************************");
+        showItemsHeader();
+    }
+
+    public static void showItemsMenu() {
+        System.out.println("\t\t\t1. Papas");
+        System.out.println("\t\t\t2. Arroz");
+        System.out.println("\t\t\t3. Carne");
+        System.out.println("\t\t\t4. Volver");
     }
 }
