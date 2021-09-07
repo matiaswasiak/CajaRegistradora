0e4a275834658d69bdf2633e5d2a9948562e91c2
matiaswasiak <matiaswasiak@gmail.com>
2021-09-07 12:02:45 -0300

    Creando vista de nuestra caja registradora

--- src/team/ed/test/Main.java
+++ src/team/ed/test/Main.java
@@ -2,20 +2,22 @@ package team.ed.test;
 
 import team.ed.objects.Product;
 import team.ed.storage.Database;
+import team.ed.views.View;
 
 public class Main {
     public static void main(String[] args) {
-        Database database = new Database();
-        Product product = database.getByIndex(0);
-        System.out.println(product);
-
-        product.setName("Pastusa");
-        product.setAmount(10);
-        product.setPrice(1000);
-
-        Product[] products = database.getAll();
-        for (Product v : products) {
-            System.out.println(v);
-        }
+//        Database database = new Database();
+//        Product product = database.getByIndex(0);
+//        System.out.println(product);
+//
+//        product.setName("Pastusa");
+//        product.setAmount(10);
+//        product.setPrice(1000);
+//
+//        Product[] products = database.getAll();
+//        for (Product v : products) {
+//            System.out.println(v);
+//        }
+        View.showHeaderPrincipal();
     }
 }
---
+++ src/team/ed/views/View.java
@@ -0,0 +1,9 @@
+package team.ed.views;
+
+public class View {
+    public static void showHeaderPrincipal() {
+        System.out.println("\t\t*************************************");
+        System.out.println("\t\t* BIENVENIDO A LA CAJA REGISTRADORA *");
+        System.out.println("\t\t*************************************");
+    }
+}
